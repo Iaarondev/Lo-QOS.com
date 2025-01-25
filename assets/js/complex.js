6282ca943bccfb867a6a746f09a32f71aec15528
Aaron Bernard <aaron.bernard.dev@gmail.com>
2025-01-25 14:29:31 -0800

    Create complex.js

---
+++ assets/js/complex.js
@@ -0,0 +1,26 @@
+// assets/js/complex.js
+export default class Complex {
+    constructor(real, imag = 0) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    add(other) {
+        return new Complex(this.real + other.real, this.imag + other.imag);
+    }
+
+    multiply(other) {
+        return new Complex(
+            this.real * other.real - this.imag * other.imag,
+            this.real * other.imag + this.imag * other.real
+        );
+    }
+
+    magnitude() {
+        return Math.sqrt(this.real ** 2 + this.imag ** 2);
+    }
+
+    toString() {
+        return `${this.real}${this.imag >= 0 ? '+' : ''}${this.imag}i`;
+    }
+}
