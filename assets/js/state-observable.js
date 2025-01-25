e431418ffa31e7111228e8affbbc3769a5ed16f4
Aaron Bernard <aaron.bernard.dev@gmail.com>
2025-01-25 14:34:19 -0800

    Update state-observable.js

--- assets/js/state-observable.js
+++ assets/js/state-observable.js
@@ -1,9 +1,53 @@
-export default class StateObservable {
-  constructor() {
-    this.observers = new Set();
-  }
+import Complex from './complex.js';
 
-  createStream(name, initialValue) {
-    console.log(`Created quantum stream: ${name}`);
-  }
+export default class StateObservable {
+    constructor() {
+        this.observers = new Map();
+        this.stateHistory = [];
+        this.errorStreams = new Set();
+    }
+
+    createStream(name, initialState) {
+        this.observers.set(name, {
+            subscribers: new Set(),
+            currentState: initialState
+        });
+    }
+
+    subscribe(streamName, callback) {
+        const stream = this.observers.get(streamName);
+        if (stream) {
+            stream.subscribers.add(callback);
+            return () => stream.subscribers.delete(callback);
+        }
+        this.reportError(`Quantum stream ${streamName} not found`);
+    }
+
+    updateStream(streamName, newState) {
+        const stream = this.observers.get(streamName);
+        if (stream) {
+            stream.currentState = newState;
+            this.stateHistory.push({
+                timestamp: Date.now(),
+                state: structuredClone(newState)
+            });
+            stream.subscribers.forEach(cb => cb(newState));
+        }
+    }
+
+    getQuantumState(streamName) {
+        return this.observers.get(streamName)?.currentState;
+    }
+
+    registerErrorStream(callback) {
+        this.errorStreams.add(callback);
+    }
+
+    reportError(message) {
+        this.errorStreams.forEach(cb => cb({
+            timestamp: Date.now(),
+            message,
+            severity: 'quantum-critical'
+        }));
+    }
 }
